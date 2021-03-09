391567ea3b67d5ae92ee936ac23b82b934f8e392
AndrewLesch <Andreykabreamz@gmail.com>
2021-03-09 23:55:15 +0300

    test

--- script/javsscript.js
+++ script/javsscript.js
@@ -2,6 +2,7 @@ const addPeopleForm = document.getElementById('add-people-form');
 const peopleListDiv = document.getElementById("people-list-div");
 let people = [];
 const PEOPLE_LOCAL_STORAGE_KEY = "people";
+const uniqueError = document.createElement("p");
 
 renderSavedPeople();
 
@@ -20,16 +21,19 @@ function addPersonToTable(person) {
   }
 }
 
-
 function addPerson(newPerson) {
   if (people.some(person => 
     newPerson.firstName === person.firstName &&
     newPerson.lastName  === person.lastName &&
     newPerson.dateOfBirth === person.dateOfBirth 
     )) {
-      alert("Введите уникального челика");
+
+      uniqueError.classList.add("error-class");
+      uniqueError.innerHTML = "Введите уникального пользователя";
+
+      document.getElementById("container").appendChild(uniqueError);
       return;
-    }
+    } 
 
     people.push(newPerson);
     addPersonToTable(newPerson);
@@ -37,6 +41,11 @@ function addPerson(newPerson) {
     console.log(localStorage);
 }
 
+function deleteError() {
+  if(uniqueError.textContent == "Введите уникального пользователя"){
+    document.getElementById("container").removeChild(uniqueError);
+  }
+}
 
 function renderSavedPeople() {
   const savedPeople = JSON.parse(localStorage.getItem(PEOPLE_LOCAL_STORAGE_KEY));
@@ -47,7 +56,6 @@ function renderSavedPeople() {
   }
 }; 
 
-
 function createTable() {
     const table = document.createElement("table");
     const tableFirstName = document.createElement("td");
@@ -55,6 +63,12 @@ function createTable() {
     const tableDateOfBirth = document.createElement("td");
     const tableWeight = document.createElement("td");
     const tr = document.createElement("tr");
+    const btn = document.createElement("BUTTON");
+    const btn1 = document.createElement("BUTTON");
+    const btn2 = document.createElement("BUTTON");
+    btn.textContent = "Sort";
+    btn1.textContent = "Sort";
+    btn2.textContent = "Sort";
 
     peopleListDiv.append(table);
     table.setAttribute('id','peopleList');
@@ -63,13 +77,15 @@ function createTable() {
     tableLastName.textContent = "Фамилия";
     tableDateOfBirth.textContent = "Дата рождения";
     tableWeight.textContent = "Вес";
+    peopleList.append(btn);
+    peopleList.append(btn1);
+    peopleList.append(btn2);
     peopleList.append(tr);
     tr.appendChild(tableFirstName);
     tr.appendChild(tableLastName);
     tr.appendChild(tableDateOfBirth);
     tr.appendChild(tableWeight);
 }
-
 
 function createTableRow(person) {
   const tr = document.createElement("tr");
@@ -98,18 +114,21 @@ function createTableRow(person) {
   deleteBtn.setAttribute('id','deleteBtn');
   deleteBtn.addEventListener("click", () => {
     tr.remove();
-    const removeIndex = people.findIndex( person => 
+    const removeIndex = people.findIndex(person => 
         person.firstName === personFirstName.textContent &&
         person.lastName === personLastName.textContent &&
         person.dateOfBirth === personDateOfBirth.textContent
       );
       console.log(removeIndex);
-      people.splice(removeIndex,1);
+
+      if (removeIndex >= 0) {
+        people.splice(removeIndex,1);
+        localStorage.setItem(PEOPLE_LOCAL_STORAGE_KEY, JSON.stringify(people));
+      }
+
       console.log(people);
-      localStorage.setItem(PEOPLE_LOCAL_STORAGE_KEY, JSON.stringify(people));
   });
 }
-
 
 function removeTableRow() {
   const table = document.getElementById("peopleList");
@@ -119,23 +138,43 @@ function removeTableRow() {
   people.shift();
 }
 
-
 function addPersonHandler(event) {
+  
   event.preventDefault();
+  
   const firstName = document.getElementById("first-name-input").value;
   const lastName = document.getElementById("last-name-input").value;
   const dateOfBirth = document.getElementById("date-input").value;
   const weight = document.getElementById("weight-input").value;
   const person = {
-    firstName : firstName,
-    lastName : lastName,
-    dateOfBirth : dateOfBirth,
-    weight : weight,
+    firstName,
+    lastName,
+    dateOfBirth,
+    weight,
   };
 
   addPerson(person);
 }
 
-
 addPeopleForm.addEventListener("submit", addPersonHandler);
 
+
+function byField(field) {
+  if(field == "dateOfBirth") {
+    return (a, b) => +a[field] > +b[field] ? 1 : -1;
+  } else {
+      return (a, b) => a[field] > b[field] ? 1 : -1;
+  }
+}
+
+function byFieldObratnaya(field) {
+  if(field == "dateOfBirth") {
+    return (a, b) => +a[field] > +b[field] ? -1 : 1;
+  } else {
+      return (a, b) => a[field] > b[field] ? -1 : 1;
+  }
+}
+
+people.sort(byField('dateOfBirth'));
+people.sort(byField('lastName'));
+people.sort(byField('firtName'));
